1c49017c272220098c797d0bc14905a930289d93
Taiga-Ebihara <45196651+Taiga-Ebihara@users.noreply.github.com>
2021-09-30 00:01:22 +0900

    add: tailwindcss

---
+++ src/utils/components.js
@@ -0,0 +1,10 @@
+import Vue from "vue"
+
+const files = require.context('@/components/common/', true, /\.vue$/)
+
+files.keys().forEach(key => {
+  const name = key.replace(/(.*\/|\.vue$)/g, '')
+  const component = files(key).default
+
+  Vue.component(name, component)
+})
